3bc8494bc7f40a7e007c8b1da1160f786a6a59df
Ricardo Jesus <ricardojpj@hotmail.com>
2023-12-19 16:57:25 +0000

    Added mqtt. Initial mqtt config

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require("express")
+const requestHandlers = require("./scripts/requestHandlers")
+const bodyParser = require("body-parser")
+const app = express()
 
 app.use("body-parser")
-app.use()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("./public"))
+
+app.listen(2000, () => {
+    console.log("Server running on http://localhost:2000/");
+});
+
+app.get("/", res.render())
+app.get("/login", requestHandlers.login)
