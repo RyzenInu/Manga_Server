55e526182c31f8efaeb859b83ad8e5708b699ade
RyzenInu <ricardojpj@hotmail.com>
2024-01-05 15:47:09 +0000

    Added files

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const requestHandlers = require("./scripts/requestHandlers")
 const bodyParser = require("body-parser")
 const app = express()
 
-app.use("body-parser")
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("./public"))
+
+app.set('view engine', 'ejs')
+app.set("views", "./views")
 
 app.listen(2000, () => {
     console.log("Server running on http://localhost:2000/");
 });
 
-app.get("/", res.render())
+app.get("/", (req, res) => {res.render("home")})
 app.get("/login", requestHandlers.login)
