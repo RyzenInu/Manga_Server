5443e86e0f94ba85608936e09040b676f5a70154
RyzenInu <ricardojpj@hotmail.com>
2024-01-10 17:17:09 +0000

    Finished Navbar. Created some page files and added some more stuff.

--- app.js
+++ app.js
@@ -14,5 +14,8 @@ app.listen(2000, () => {
     console.log("Server running on http://localhost:2000/");
 });
 
-app.get("/", (req, res) => {res.render("home")})
-app.get("/login", requestHandlers.login)
+app.get("/", (req, res) => {res.redirect("/manga")})
+app.get("/manga", (req, res) => {res.render("manga")})
+app.get("/home", (req, res) => {res.render("home")})
+app.get("/equipment", (req, res) => {res.render("equipment")})
+app.get("/stats", (req, res) => {res.render("stats")})
--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,16 +1,58 @@
-var userProfile = document.getElementsByClassName("navbar-user")[0];;
+const currentUrl = (window.location.pathname).slice(1);
 
 window.onload = (event) => {
-    
+    setActiveBtn();
 }
 
+function setActiveBtn() {
+    let btns = document.getElementsByClassName("navbar-button");
+    switch (currentUrl) {
+        case "home":
+            btns.item(0).classList.toggle("active");
+            break;
+        case "equipment":
+            btns.item(1).classList.toggle("active");
+            break;
+        case "stats":
+            btns.item(2).classList.toggle("active");
+            break;
+
+    }
+}
+
+var userProfile = document.getElementById("navbar-user");
+
 userProfile.addEventListener("click", (e) => {
-    let currentSize = {"height":userProfile.offsetHeight, "width":userProfile.offsetWidth}
-    console.log(currentSize)
-    //userProfile.style.maxWidth = currentSize.width;
-    userProfile.style.width = "100%";
-    //userProfile.style.height = currentSize.width;
-    userProfile.style.maxWidth = "1000px";
-    userProfile.style.maxHeight = "500px";
-    userProfile.style.position = "absolute";
+    toggleUserPopup()
+    toggleBlurBg();
+    /*userProfile.style.position = "absolute";
+    userProfile.style.top = pos.top + "px";
+    userProfile.style.left = pos.left + "px";
+
+    userProfile.style.transform = "translate(-50%, -50%)";
+    userProfile.style.top = "50%";
+    userProfile.style.left = "50%";*/
 })
+
+var userPopupCloseBtn = document.getElementById("userPopupCloseBtn");
+
+userPopupCloseBtn.addEventListener("click", (e) => {
+    toggleUserPopup();
+    toggleBlurBg();
+})
+
+var userPopup = document.getElementById("userPopup")
+
+userPopup.style.top = userProfile.offsetTop + "px";
+userPopup.style.left = userProfile.offsetLeft + "px";
+
+function toggleUserPopup() {
+    userProfile.classList.toggle("o0")
+    userPopup.classList.toggle("popupActive")
+}
+
+var blurBg = document.getElementById("blurBg")
+
+function toggleBlurBg() {
+    blurBg.classList.toggle("active")
+}
