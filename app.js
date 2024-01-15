3f184fe9662ec82cf0f24b4b82800dca6f451ea0
RyzenInu <ricardojpj@hotmail.com>
2024-01-15 13:07:16 +0000

    decelopment 👍

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ app.use(express.static("./public"))
 app.set('view engine', 'ejs')
 app.set("views", "./views")
 
-const mqtt = new MqttHandler("127.23.5.214", "manga_server", "pwdam", "password");
+const mqtt = new MqttHandler("mqtt://127.23.5.214", "manga_server", "pwdam", "password");
 mqtt.connect();
 
-app.get("/", (req, res) => {res.redirect("/manga")})
-app.get("/manga", (req, res) => {res.render("manga")})
-app.get("/home", (req, res) => {res.render("home")})
-app.get("/equipment", (req, res) => {res.render("equipment")})
-app.get("/stats", (req, res) => {res.render("stats")})
+app.get("/", (req, res) => { res.redirect("/manga") })
+app.get("/manga", (req, res) => { res.render("manga") })
+app.get("/home", (req, res) => { res.render("home") })
+app.get("/equipment", (req, res) => { res.render("equipment") })
+app.get("/stats", (req, res) => { res.render("stats") })
+app.get("/login", (req, res) => { res.render("login") })
 
 app.listen(2000, () => {
     console.log("Server running on http://localhost:2000/");
---
+++ public/scripts/manga.js
@@ -0,0 +1,21 @@
+var scrollPrevBtn = document.getElementById("prevSectionIndicator");
+var scrollNextBtn = document.getElementById("nextSectionIndicator");
+var scrollable = document.getElementById('scrollable');
+
+scrollPrevBtn.addEventListener("click", (event) => {
+    scrollable.scrollTo(0, (scrollable.scrollTop - document.body.clientHeight));
+});
+
+scrollNextBtn.addEventListener("click", (event) => {
+    scrollable.scrollTo(0, (scrollable.scrollTop + document.body.clientHeight));
+});
+
+scrollable.addEventListener("scroll", () => {
+    if (scrollable.scrollTop < document.body.clientHeight) {
+        scrollPrevBtn.style.opacity = "0";
+        scrollPrevBtn.style.visibility = "collapse";
+    } else if (scrollable.scrollTop >= document.body.clientHeight){
+        scrollPrevBtn.style.opacity = 1;
+        scrollPrevBtn.style.visibility = "visible";
+    }
+})
