7aa8b6bd8751a9e1e5f7d57287a660ed0bc8d1f9
RyzenInu <ricardojpj@hotmail.com>
2024-02-07 15:19:29 +0000

    Commented random number generator

--- app.js
+++ app.js
@@ -40,37 +40,38 @@ app.post("/mqtt/send/:topic/:device/:message", (req, res) => {
 });
 //mqtt.sendMessage("peltierControl", "ON");
 
+// Test function to create fictional, random, data and send it to the database.
+//
+// setInterval(async () => {
+//     let randomNum = (Math.random() * (22.0 - 18.0) + 18.0).toFixed(2);
+//     let randomVol = (Math.random() * (0.0 - 0.35) + 0.35).toFixed(3);
+//     //mqtt.sendMessage("temp", randomNum);
+//     //mqtt.sendMessage("volume", randomVol);
 
-setInterval(async () => {
-    let randomNum = (Math.random() * (22.0 - 18.0) + 18.0).toFixed(2);
-    let randomVol = (Math.random() * (0.0 - 0.35) + 0.35).toFixed(3);
-    //mqtt.sendMessage("temp", randomNum);
-    //mqtt.sendMessage("volume", randomVol);
-
-    let con = mysql.createConnection(options.database)
-    con.connect((err => {
-        if (err) {
-            console.log(err);
-            con.end();
-        } else {
-            let query = mysql.format("insert into temp(valor, id_recipiente) values(?,?)", [randomNum, 1]);
-            con.query(query, (err, result) => {
-                if (err) {
-                    console.log(err);
-                    con.end();
-                } else {
-                    query = mysql.format("insert into volume(valor, id_recipiente) values(?,?)", [randomVol, 1]);
-                    con.query(query, (err, result) => {
-                        if (err) {
-                            console.log(err);
-                            con.end();
-                        } else { con.end() }
-                    })
-                }
-            })
-        }
-    }))
-}, 3000);
+//     let con = mysql.createConnection(options.database)
+//     con.connect((err => {
+//         if (err) {
+//             console.log(err);
+//             con.end();
+//         } else {
+//             let query = mysql.format("insert into temp(valor, id_recipiente) values(?,?)", [randomNum, 1]);
+//             con.query(query, (err, result) => {
+//                 if (err) {
+//                     console.log(err);
+//                     con.end();
+//                 } else {
+//                     query = mysql.format("insert into volume(valor, id_recipiente) values(?,?)", [randomVol, 1]);
+//                     con.query(query, (err, result) => {
+//                         if (err) {
+//                             console.log(err);
+//                             con.end();
+//                         } else { con.end() }
+//                     })
+//                 }
+//             })
+//         }
+//     }))
+// }, 3000);
 
 // Page Routing
 app.get("/", (req, res) => { res.redirect("/manga") })
